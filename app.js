c3e393cf6cefc56d07fdd0fe773d479de972b8d0
DragicevicS <steffan95@live.com>
2023-04-11 19:22:41 +0200

    Add a function for adding and removing books

--- app.js
+++ app.js
@@ -1,73 +1,105 @@
 const myLibrary = [];
 
+//constructor for 'Book' object
 function Book(title, author, pages, read) {
-  this.title = title
-  this.author = author
-  this.pages = pages
-  this.read = read
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  if (read == true) this.read = 'read';
+  if (read == false) this.read = 'not read'
 }
 
+//manually adding few books
 function addBookToLibrary() {
   myLibrary.push(...arguments);
   console.log(myLibrary);
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 296, 'not read');
-
-const got = new Book('A Game of Thrones', 'George R.R. Martin', 245, 'read');
-
-const harryPotter = new Book('Harry Potter', 'J.K. Rowling', 233, 'not read');
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 296, false);
+const got = new Book('A Game of Thrones', 'George R.R. Martin', 245, true);
+const harryPotter = new Book('Harry Potter', 'J.K. Rowling', 233, false);
 
 addBookToLibrary(hobbit, got, harryPotter);
 
+//using loop to list all the books in the 'myLibrary' array
 const tableBody = document.querySelector('tbody');
 
-
-
 for (let i=0; i < myLibrary.length; i++) {
-  const tableRow = document.createElement('tr');
-  const tableCell0 = tableRow.insertCell(0);
-  const tableCell1 = tableRow.insertCell(1);
-  const tableCell2 = tableRow.insertCell(2);
-  const tableCell3 = tableRow.insertCell(3);
-  const tableCell4 = tableRow.insertCell(4);
-  const tableCell5 = tableRow.insertCell(5);
-  tableCell4.setAttribute('class', 'read');
-  tableBody.appendChild(tableRow);
-  tableCell0.textContent = i+1;
-  tableCell1.textContent = myLibrary[i].title;
-  tableCell2.textContent = myLibrary[i].author;
-  tableCell3.textContent = myLibrary[i].pages;
-  tableCell4.textContent = myLibrary[i].read;
-  const removeBtn = document.createElement('button');
-  const toggleReadBtn = document.createElement('button');
-  toggleReadBtn.setAttribute('class', 'toggle');
-  removeBtn.textContent = 'X';
-  toggleReadBtn.textContent = 'Toggle read';
-  tableCell5.appendChild(removeBtn);
-  tableCell5.appendChild(toggleReadBtn);
+  tableBody.innerHTML += 
+  `
+  <tr>
+    <td>${myLibrary[i].title}</td>
+    <td>${myLibrary[i].author}</td>
+    <td>${myLibrary[i].pages}</td>
+    <td class="read">${myLibrary[i].read}</td>
+    <td>
+      <button class="remove">X</button>
+      <button class="toggle">Toggle read</button>
+    </td>
+  </tr>
+  `;
 };
 
-const toggle = document.querySelector('.toggle');
-const td4 = document.querySelector('.read');
-console.log(td4);
+//removing table row button
+function removeRow(e) {
+  if (!e.target.classList.contains("remove")) return;
+  e.target.closest('tr').remove();
+}
 
-toggle.addEventListener('click', () => {
-  if (td4.textContent === 'not read') td4.textContent = 'read';
-  else td4.textContent = 'not read';
-});
+tableBody.addEventListener('click', removeRow);
 
-const addNewBook = document.querySelector('#addNewBook');
+//toggle read button
+const tdRead = document.querySelector('.read');
+function toggleRead(e) {
+  if (!e.target.classList.contains("toggle")) return;
+  console.log(e);
+};
+
+tableBody.addEventListener('click', toggleRead);
+
+//toggle form visibility 
+const toggleForm = document.querySelector('#toggleForm');
 const form = document.querySelector('#addBookForm')
-addNewBook.addEventListener('click', () => {
-  if (form.style.visibility == 'hidden') {
-    form.style.visibility = 'visible';
-    addNewBook.textContent = 'Hide form';
+toggleForm.addEventListener('click', () => {
+  if (form.style.display == 'none') {
+    form.style.display = 'grid';
+    toggleForm.textContent = 'Hide form';
   } else {
-    form.style.visibility = 'hidden';
-    addNewBook.textContent = 'Add new book';
+    form.style.display = 'none';
+    toggleForm.textContent = 'Add new book';
   };
 })
+
+//adding new 'book' to the 'myLibrary' array
+function addNewBook(e) {
+  e.preventDefault();
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  const read = document.querySelector('input[name="read"]:checked').value;
+
+  const newBook = new Book(title, author, pages, read);
+  addBookToLibrary(newBook);
+  console.log(read);
+  console.log(newBook.read);
+  tableBody.innerHTML +=
+  `
+  <tr>
+    <td>${newBook.title}</td>
+    <td>${newBook.author}</td>
+    <td>${newBook.pages}</td>
+    <td class="read">${newBook.read}</td>
+    <td>
+      <button class="remove">X</button>
+      <button class="toggle">Toggle read</button>
+    </td>
+  </tr>
+  `;
+  form.style.display = 'none';
+  toggleForm.textContent = 'Add new book';
+};
+
+form.addEventListener('submit', addNewBook);
 
 
 
